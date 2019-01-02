8efa911b04853a70d9fc6ea3a3f6c389bc5056f8
yinm <yinm.cminor9@gmail.com>
2019-01-02 14:25:00 +0900

    Add variation of images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const httpstatus = require('./src/httpstatus')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -7,21 +8,43 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/', (req, res) => {
-  const text = req.body.text
+  let message = {}
 
-  if (!/^\d+$/.test(text)) {
-    res.send('Error: enter a valid status code, such as 200')
-    return
+  if (req.body.text) {
+    const code = req.body.text
+
+    if (!/^\d+$/.test(code)) {
+      res.send(':sob: U R DOIN IT WRONG. Enter a status code like 200')
+      return
+    }
+
+    const status = httpstatus[code]
+    if (!status) {
+      res.send(`Bummere, ${code} is not a HTTP status code :scream_cat:`)
+    }
+
+    message = {
+      response_type: 'in_channel',
+      attachments: [
+        {
+          pretext: `${code}: ${status}`,
+          image_url: `https://http.cat/${code}`
+        }
+      ]
+    }
+  } else {
+    message = {
+      response_type: 'ephemeral',
+      text: ':cat: How to use `/httpstatus` command:',
+      attachments: [
+        {
+          text: 'Type a status code after the command, _e.g._ `/httpstatus 404`'
+        }
+      ]
+    }
   }
 
-  const data = {
-    response_type: 'in_channel',
-    text: '302: Found',
-    attachments: [{
-      image_url: 'https://http.cat/302.jpg'
-    }]
-  }
-  res.json(data)
+  res.json(message)
 })
 
 const server = app.listen(3000, () => {
